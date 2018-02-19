8ae0ebe5c9c6d9016432414e3cf43681ab70c7b7
ignacio <ignaciojluque@gmail.com>
2018-02-18 22:26:53 -0300

    ya no paso parametros por URL, quedo corregido.

--- desafioML.py
+++ desafioML.py
@@ -1,7 +1,9 @@
 ## Desafio Mercado Libre , Ignacio Luque
 
+import json
 import os
 import flask
+from flask import request, jsonify
 import httplib2
 from apiclient import discovery
 from apiclient.http import MediaIoBaseDownload, MediaFileUpload
@@ -11,14 +13,17 @@ from oauth2client.file import Storage
 
 app = flask.Flask(__name__)
 
-@app.route('/search-in-doc/<palabra>/<ident>/',methods=['GET']) 
-def index(palabra,ident):# funcion que busca una palabra en en el documento indicado(ambos pasados por parametro)
+@app.route('/search-in-doc/<ident>/',methods=['GET']) 
+def index(ident):# funcion que busca una palabra en en el documento indicado(ambos pasados por parametro)
     credentials = get_credentials() #se obtienen credenciales para usar apis de google
     if credentials == False:
 		return flask.redirect(flask.url_for('oauth2callback'))# se debe ingresar en la cuenta de google drive
     elif credentials.access_token_expired:
 		return flask.redirect(flask.url_for('oauth2callback'))# expiro la sesion y se debe ingresar nuevamente
     else:
+		palabra=request.args.get('word')
+		if palabra is None:
+			return "Falta pasar palabra como parametro, por favor agregue: ?word=palabra_deseada" # no se paso palabra por parametro 			
 		all_files = fetch( "fullText contains '%s'"% palabra, sort='modifiedTime desc')# se buscan todos los archivos que contengan la palabra pasada por parametro
 		for file in all_files:
 			ide = file['id']
@@ -28,23 +33,24 @@ def index(palabra,ident):# funcion que busca una palabra en en el documento indi
 				return "HTTP/1.1 200 OK"
 		return "HTTP/1.1 404 Not Found" # si llego hasta aca es porque no se encontro el documento
 
-@app.route('/crear-doc/<titulo>/<descripcion>/',methods=['GET'])
-def crear_doc(titulo,descripcion):
-	file_metadata = {
-    'name' : titulo,
-	'title' : titulo,
-	'description' : descripcion,
-    'mimeType': 'application/vnd.google-apps.document'
-	}
+@app.route('/crear-doc/', methods=['GET', 'POST'])
+def crear_doc():
+	file_metadata = request.get_json()	
+	titulo = file_metadata.get("titulo")
+	descripcion = file_metadata.get("descripcion")
+	
+	if file_metadata is None or titulo is None or descripcion is None:# si no se recibio el archivo json o los parametros titulo y descripcion son nulos , entonces devuelve error 400
+		return "HTTP/1.1 400"
 	credentials = get_credentials()
 	http = credentials.authorize(httplib2.Http())
 	service = discovery.build('drive', 'v3', http=http)
 	file = service.files().create(body=file_metadata, # se crea el documento con los datos pasados por parametro
                                     fields='id').execute()
+	
 	if file is None:
-		return "HTTP/1.1 500"
-	return "HTTP/1.1 200 OK \n El id es: {!s},  el titulo del documento es: {!s},  descripcion: {!s}".format(file.get('id'),titulo, descripcion)
-
+		return "HTTP/1.1 500" 
+	
+	return "HTTP/1.1 200 OK" + " id: %s propiedades: " % file.get('id') +  str(file_metadata)
 
 @app.route('/oauth2callback')
 def oauth2callback():
